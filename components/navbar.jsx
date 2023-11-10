214a170d5054f920fbbaebfab6374af668ee3cfd
romiiiiii <mzeba16@gmail.com>
2023-11-11 02:33:15 +0600

    touch up

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
 
     return(
-    <div className="bg-blue-800 justify-center md:w-full ">
-        <h1 className="bg-blue-500 md:w-full justify-center h-16 ">navbar</h1>
-    </div>
+        <div className="bg-blue-500  text-white	 flex flex-col items-center">
+        <h1 className=" w-full ">navbar</h1>
+        <div className="flex">
+          <Link href="/MedicalRecordEntry">
+            <p className=" w-full text-center p-4 font-bold ">EHR Entry Form</p>
+          </Link>
+          <Link href="/PatientHistory">
+            <p className=" w-full  text-center p-4 font-bold">Search patient history</p>
+          </Link>
+        </div>
+      </div>
+      
     );
 };
 
